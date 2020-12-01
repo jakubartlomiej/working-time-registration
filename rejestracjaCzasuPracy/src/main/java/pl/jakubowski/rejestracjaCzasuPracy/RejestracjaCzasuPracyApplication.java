4e3ac3f2f18ef97b1c1d0788698020dc051fb5e5
bjakubowski <bartakj141@wp.pl>
2020-12-01 08:49:20 +0100

    create project

---
+++ rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/RejestracjaCzasuPracyApplication.java
@@ -0,0 +1,13 @@
+package pl.jakubowski.rejestracjaCzasuPracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RejestracjaCzasuPracyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RejestracjaCzasuPracyApplication.class, args);
+	}
+
+}
---
+++ rejestracjaCzasuPracy/src/test/java/pl/jakubowski/rejestracjaCzasuPracy/RejestracjaCzasuPracyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubowski.rejestracjaCzasuPracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RejestracjaCzasuPracyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
