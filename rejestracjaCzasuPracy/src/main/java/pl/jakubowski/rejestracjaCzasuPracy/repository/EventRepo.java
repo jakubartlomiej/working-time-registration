25f37e4e70fb889001ad762702f4d5c758f0e584
bjakubowski <bartakj141@wp.pl>
2020-12-01 11:15:52 +0100

    added eventRepo interface

---
+++ rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/repository/EventRepo.java
@@ -0,0 +1,9 @@
+package pl.jakubowski.rejestracjaCzasuPracy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.jakubowski.rejestracjaCzasuPracy.entity.Event;
+
+@Repository
+public interface EventRepo extends JpaRepository<Event, Long> {
+}
