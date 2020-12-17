143a0150268bcb6e4f51465e9a6046b49098669e
bjakubowski <bartakj141@wp.pl>
2020-12-17 13:18:18 +0100

    added and implementing method findByEmployeeIdAndDateBetween

--- rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/manager/EventManager.java
+++ rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/manager/EventManager.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import pl.jakubowski.rejestracjaCzasuPracy.entity.Event;
 import pl.jakubowski.rejestracjaCzasuPracy.repository.EventRepo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -25,6 +27,10 @@ public class EventManager {
 
     public void addEvent(Event event) {
         eventRepo.save(event);
+    }
+
+    public Iterable<Event> findByEmployeeIdAndDateBetween(long employeeId, LocalDateTime dateStart, LocalDateTime dateEnd) {
+        return eventRepo.findByEmployeeIdAndDateBetween(employeeId, dateStart, dateEnd);
     }
 
 }
--- rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/repository/EventRepo.java
+++ rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/repository/EventRepo.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.jakubowski.rejestracjaCzasuPracy.entity.Event;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Repository
 public interface EventRepo extends JpaRepository<Event, Long> {
+
+    Iterable<Event> findByEmployeeIdAndDateBetween(Long employee_id, LocalDateTime date, LocalDateTime date2);
 }
