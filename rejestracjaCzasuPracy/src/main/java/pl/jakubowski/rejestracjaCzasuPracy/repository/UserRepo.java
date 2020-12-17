1e2c4d81f0d098e89fb920c5a9b0b6a3910220b2
bjakubowski <bartakj141@wp.pl>
2020-12-17 09:22:04 +0100

    added query update login

--- rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/repository/UserRepo.java
+++ rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/repository/UserRepo.java
@@ -1,8 +1,11 @@
 package pl.jakubowski.rejestracjaCzasuPracy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import pl.jakubowski.rejestracjaCzasuPracy.entity.Employee;
+import org.springframework.transaction.annotation.Transactional;
 import pl.jakubowski.rejestracjaCzasuPracy.entity.User;
 
 import java.util.Optional;
@@ -12,5 +15,9 @@ public interface UserRepo extends JpaRepository<User, Long> {
 
     Optional<User> findByLogin(String login);
     Optional<User> findByEmployeeId(Long id);
+    @Modifying
+    @Transactional
+    @Query(value = "update User u set u.login = :login where u.id = :id")
+    int updateLoginForId(@Param("login") String login, @Param("id") long id);
 
 }
