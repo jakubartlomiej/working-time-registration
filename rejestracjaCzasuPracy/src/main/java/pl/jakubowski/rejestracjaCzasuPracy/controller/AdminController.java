54dbf736ae8646bbd195505887a09c44465bf897
bjakubowski <bartakj141@wp.pl>
2020-12-17 09:23:15 +0100

    added method updateEmployee, addLogin, resetPassword, grant and delete role, disabled and enabled card

--- rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/controller/AdminController.java
+++ rejestracjaCzasuPracy/src/main/java/pl/jakubowski/rejestracjaCzasuPracy/controller/AdminController.java
@@ -12,6 +12,7 @@ import pl.jakubowski.rejestracjaCzasuPracy.manager.RoleManager;
 import pl.jakubowski.rejestracjaCzasuPracy.manager.UserManager;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.Collections;
 import java.util.HashMap;
 
@@ -101,11 +102,172 @@ public class AdminController {
     }
 
     @PostMapping("/user/update")
-    public String updateEmployee(@ModelAttribute Employee updateEmployee, @ModelAttribute User updateUser, @RequestParam(defaultValue = "false") boolean myCheckBox) {
-        if (myCheckBox) {
-            userManager.save(updateUser);
+    public String updateEmployee(@ModelAttribute Employee updateEmployee, BindingResult bindingResult, @ModelAttribute User updateUser, Model model) {
+        boolean error = false;
+        HashMap<String, String> errors = new HashMap<>();
+        if (updateUser != null) {
+            if (userManager.findByLogin(updateUser.getLogin()).isPresent()) {
+                if (!userManager.findByLogin(updateUser.getLogin()).get().getUsername().equals(updateUser.getLogin())) {
+                    errors.put("loginExist", "Użytkownik o takim loginie już istnieje");
+                    error = true;
+                }
+            }
+            if (bindingResult.hasErrors()) {
+                error = true;
+            }
+            if (error) {
+                model.addAttribute("uniqueErrors", errors);
+                return "admin/user/user-info";
+            }
+            if (userManager.findByEmployeeId(updateEmployee.getId()).isPresent()) {
+                User user = userManager.findByEmployeeId(updateEmployee.getId()).get();
+                userManager.updateLoginForId(updateUser.getLogin(), user.getId());
+            }
+        } else {
+            if (employeeManager.findByCardNumber(updateEmployee.getCardNumber()).isPresent()) {
+                if (!employeeManager.findByCardNumber(updateEmployee.getCardNumber()).get().getCardNumber().equals(updateEmployee.getCardNumber())) {
+                    errors.put("cardNumberExist", "Podany numer karty istnieje w systemie");
+                    error = true;
+                }
+            }
+            if (bindingResult.hasErrors()) {
+                error = true;
+            }
+            if (error) {
+                model.addAttribute("uniqueErrors", errors);
+                return "admin/user/user-info";
+            }
         }
-        employeeManager.save(updateEmployee);
+        if (employeeManager.findById(updateEmployee.getId()).isPresent()) {
+            Employee employee = employeeManager.findById(updateEmployee.getId()).get();
+            employee.setFirstName(updateEmployee.getFirstName());
+            employee.setLastName(updateEmployee.getLastName());
+            employee.setCardNumber(updateEmployee.getCardNumber());
+            employeeManager.save(employee);
+        }
         return "redirect:/admin/users";
+    }
+
+    @GetMapping("/user/{employeeId}/add-login")
+    public String addLoginForm(@PathVariable long employeeId, Model model) {
+        model.addAttribute("employee", employeeManager.findById(employeeId).orElseThrow(() -> new RuntimeException("Pracwonk nie znaleziony")));
+        model.addAttribute("user", new User());
+        return "admin/user/add-login";
+    }
+
+    @PostMapping("user/{employeeId}/add-login")
+    public String submitLoginUser(@ModelAttribute @Valid User user, BindingResult bindingResult, @PathVariable long employeeId, @RequestParam String roleName, Model model) {
+        boolean error = false;
+        if (userManager.findByLogin(user.getLogin()).isPresent()) {
+            error = true;
+            model.addAttribute("loginExist", "Taki login już isteniej");
+        }
+        if (bindingResult.hasErrors()) {
+            error = true;
+        }
+
+        if (error) {
+            model.addAttribute("employee", employeeManager.findById(employeeId).orElseThrow(() -> new RuntimeException("Pracwonk nie znaleziony")));
+            return "admin/user/add-login";
+        }
+        Role role = roleManager.findByRoleName(roleName).orElse(null);
+        user.setRoles(Collections.singletonList(role));
+        userManager.save(user);
+        if (employeeManager.findById(employeeId).isPresent()) {
+            Employee employee = employeeManager.findById(employeeId).get();
+            employee.setUser(user);
+            employeeManager.save(employee);
+        }
+        return "redirect:/admin/user/" + employeeId;
+    }
+
+    @GetMapping("/user/{employeeId}/password")
+    public String resetPasswordForm(@PathVariable long employeeId, Model model) {
+        model.addAttribute("user", userManager.findByEmployeeId(employeeId).orElseThrow(() -> new RuntimeException("User nie znaleziony")));
+        return "admin/password/password";
+    }
+
+    @PostMapping("/user/{employeeId}/password")
+    public String subminPasswordReset(@RequestParam @Min(value = 8, message = "Minimum 8 znaków") String newPassword, @PathVariable long employeeId, Model model) {
+        if (newPassword.length() < 8) {
+            model.addAttribute("error", "Minimum 8 znaków");
+            model.addAttribute("user", userManager.findByEmployeeId(employeeId).orElseThrow(() -> new RuntimeException("User nie znaleziony")));
+            return "admin/password/password";
+        } else {
+            if (userManager.findByEmployeeId(employeeId).isPresent()) {
+                User user = userManager.findByEmployeeId(employeeId).get();
+                user.setPassword(newPassword);
+                userManager.save(user);
+            }
+        }
+        return "redirect:/admin/user/" + employeeId;
+    }
+
+    @GetMapping("/user/{employeeId}/role-grant")
+    public String grantRoleForm(@PathVariable long employeeId, Model model) {
+        model.addAttribute("roles", roleManager.findRoleToGrantByEmployeeId(employeeId));
+        model.addAttribute("role", new Role());
+        return "admin/role/grant";
+    }
+
+    @PostMapping("/user/{employeeId}/role-grant")
+    public String submitGrantRole(@PathVariable long employeeId, Role role) {
+        userManager.addRole(role, employeeId);
+        return "redirect:/admin/user/" + employeeId;
+    }
+
+    @GetMapping("/user/{employeeId}/role-delete")
+    public String deleteRoleForm(Model model, @PathVariable long employeeId) {
+        model.addAttribute("user", userManager.findByEmployeeId(employeeId).orElseThrow(() -> new RuntimeException("User nie znaleziony")));
+        model.addAttribute("role", new Role());
+        return "admin/role/delete";
+    }
+
+    @PostMapping("/user/{employeeId}/role-delete")
+    public String submitDeleteRole(@PathVariable long employeeId, Role role) {
+        userManager.deleteRole(role, employeeId);
+        return "redirect:/admin/user/" + employeeId;
+    }
+
+    @GetMapping("/user/{employeeId}/user-delete")
+    public String confirmDeleteUser(@PathVariable long employeeId, Model model) {
+        model.addAttribute("user", userManager.findByEmployeeId(employeeId).orElseThrow(() -> new RuntimeException("User nie znaleziony")));
+        return "admin/user/delete";
+    }
+
+    @PostMapping("/user/{employeeId}/user-delete")
+    public String deleteUserAccount(@PathVariable long employeeId) {
+        if (employeeManager.findById(employeeId).isPresent()) {
+            Employee employee = employeeManager.findById(employeeId).get();
+            if (userManager.findByEmployeeId(employeeId).isPresent()) {
+                long tempUserId = employee.getUser().getId();
+                employee.setUser(null);
+                employeeManager.save(employee);
+                userManager.deleteAllRole(tempUserId);
+                userManager.deleteById(tempUserId);
+
+            }
+        }
+        return "redirect:/admin/user/" + employeeId;
+    }
+
+    @GetMapping("/user/{employeeId}/card-disabled")
+    public String disabledCard(@PathVariable long employeeId) {
+        Employee employee = employeeManager.findById(employeeId).orElseThrow(() -> new RuntimeException("Pracownik nie znaleziony"));
+        if (employee != null) {
+            employee.setActive(false);
+            employeeManager.save(employee);
+        }
+        return "redirect:/admin/user/" + employeeId;
+    }
+
+    @GetMapping("/user/{employeeId}/card-enabled")
+    public String enabledCard(@PathVariable long employeeId) {
+        Employee employee = employeeManager.findById(employeeId).orElseThrow(() -> new RuntimeException("Pracownik nie znaleziony"));
+        if (employee != null) {
+            employee.setActive(true);
+            employeeManager.save(employee);
+        }
+        return "redirect:/admin/user/" + employeeId;
     }
 }
